b547330b66f649f1f43fbc74d8213c82d2590906
NiklasRitter <n.ritter2900@gmail.com>
2023-02-17 18:18:21 +0100

    fix: comments

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,11 @@
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import express from 'express'
-import helmet from 'helmet';
-import { CORS_ORIGIN } from './constants';
-import userRoute from './modules/user/user.route';
-import { connectToDatabase, disconnectFromDatabase } from './utils/database';
-import logger from './utils/logger';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import { CORS_ORIGIN } from "./constants";
+import userRoute from "./modules/user/user.route";
+import { connectToDatabase, disconnectFromDatabase } from "./utils/database";
+import logger from "./utils/logger";
 
 const PORT = process.env.PORT || 4000;
 
@@ -13,32 +13,35 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     origin: CORS_ORIGIN,
-    credentials: true, 
-}))
+    credentials: true,
+  })
+);
 app.use(helmet());
 
-app.use('/api/users', userRoute)
+app.use("/api/users", userRoute);
 
 const server = app.listen(PORT, async () => {
-    await connectToDatabase();
-    logger.info(`Server listening at htp://localhost:${PORT}`);
-})
+  await connectToDatabase();
+  logger.info(`Server listening at htp://localhost:${PORT}`);
+});
 
-const signals = ["SIGTERM", "SIGINT"]; //SIGTERM: kill signal //SIGINT: Ctrl+C
+//SIGTERM: kill signal //SIGINT: Ctrl+C
+const signals = ["SIGTERM", "SIGINT"];
 
 function gracefulShutdown(signal: string) {
-    process.on(signal, async () => {
-        server.close();
-        logger.info("Close server with signal", signal);
-    
-        await disconnectFromDatabase();
-    
-        process.exit(0);
-      });
+  process.on(signal, async () => {
+    server.close();
+    logger.info("Close server with signal", signal);
+
+    await disconnectFromDatabase();
+
+    process.exit(0);
+  });
 }
 
 for (let i = 0; i < signals.length; i++) {
-    gracefulShutdown(signals[i]);
-  }
+  gracefulShutdown(signals[i]);
+}
--- server/src/utils/database.ts
+++ server/src/utils/database.ts
@@ -2,20 +2,21 @@ import mongoose from "mongoose";
 import logger from "./logger";
 
 const DB_CONNECTION_STRING =
-process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/video-streaming-platform"; //TODO: Do a process.env DB Connectionsstring  
+  process.env.DB_CONNECTION_STRING ||
+  "mongodb://127.0.0.1:27017/video-streaming-platform"; //TODO: Do a process.env DB Connectionsstring
 
 export async function connectToDatabase() {
-    try {
-        await mongoose.connect(DB_CONNECTION_STRING);
-        logger.info("Connected to database");
-      } catch (e) {
-        logger.error(e, "Failed to connect to database.");
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(DB_CONNECTION_STRING);
+    logger.info("Connected to database");
+  } catch (e) {
+    logger.error(e, "Failed to connect to database.");
+    process.exit(1);
+  }
 }
 
 export async function disconnectFromDatabase() {
-    await mongoose.connection.close();
-    logger.info("Disconnect from database");
-    return;
-  }
+  await mongoose.connection.close();
+  logger.info("Disconnect from database");
+  return;
+}
