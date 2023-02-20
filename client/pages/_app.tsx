680bfaef8707dff8e212a8e6eb65a3efeea1a6d7
NiklasRitter <n.ritter2900@gmail.com>
2023-02-20 12:39:21 +0100

    feat: main page

---
+++ client/pages/_app.tsx
@@ -0,0 +1,56 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import Head from "next/head";
+import { NotificationsProvider } from "@mantine/notifications";
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { CurrentUserContextProvider } from "@/context/currentuser";
+
+const queryClient = new QueryClient();
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  // Use layout defined at page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <>
+      <Head>
+        <title>Video Streaming</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "dark",
+        }}
+      >
+        <NotificationsProvider>
+          <QueryClientProvider client={queryClient}>
+            <CurrentUserContextProvider>
+              {getLayout(
+                <main>
+                  <Component {...pageProps} />
+                </main>
+              )}
+            </CurrentUserContextProvider>
+          </QueryClientProvider>
+        </NotificationsProvider>
+      </MantineProvider>
+    </>
+  );
+}
