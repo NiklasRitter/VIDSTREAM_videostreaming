f042865508ab2bc71989b04e1fdff69cd164378b
NiklasRitter <n.ritter2900@gmail.com>
2023-02-20 12:41:34 +0100

    feat: video and currentUser interface

---
+++ client/types/index.ts
@@ -0,0 +1,23 @@
+export interface Video {
+  _id: string;
+  owner: string;
+  published: boolean;
+  videoId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+  extension: string;
+  description: string;
+  title: string;
+}
+
+export enum QueryKeys {
+  currentUser = "currentUser",
+  videos = "videos",
+}
+
+export interface CurrentUser {
+  _id: string;
+  email: string;
+  username: string;
+}
